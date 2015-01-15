752a09aaf38602237601c6a38f5b8f0c90b403c6
Mark Woosey <git@mrkw.se>
2015-01-15 20:40:22 +0000

    Initial Commit

---
+++ LemonadeStand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LemonadeStand
+//
+//  Created by Mark Woosey on 15/01/2015.
+//  Copyright (c) 2015 Mark Woosey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
